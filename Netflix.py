6e265e10b1681ce45753a2721f7da6cad70e4d60
Nelma Perera Rodriguez <npererar@cs.utexas.edu>
2015-06-24 04:48:15 -0500

    Final Version

--- Netflix.py
+++ Netflix.py
@@ -6,89 +6,98 @@ import re
 import io
 import ctypes
 import math
+import json
 
 # ------------
 # netflix_avg_movie
 # ------------
 
 def netflix_avg_movie (movie_id) :
-    movie_file = netflix_read_movie_files (movie_id)
-    sum_movie = 0
-    count_users = 0
-    t2 = movie_file.readline()
-    t2 = movie_file.readline()
-    while t2 != '\n' and t2 != '' : 
-        sum_movie += float(t2[-13:-12])
-        count_users += 1
-        t2 = movie_file.readline() 
-    assert (count_users != 0)
-    avg =  sum_movie / float(count_users)
-    movie_file.close()
+    '''
+        param: movie_id
+        description: The variable data has the access to the cache.
+                     (keys,values) = (movies ID,movie's average)
+        return: The overall average of the movie corresponding to the movie_id
+    '''
+    assert movie_id != {}
+
+    url = "http://www.cs.utexas.edu/~ebanner/netflix-tests/BRG564-Average_Movie_Rating_Cache.json"
+    r = urllib.request.urlopen(url)
+    data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
+
+    avg = data[movie_id]
+
+    assert 1.0 <= avg <= 5.0
     return avg
+
 
 # ------------
 # netflix_avg_user
 # ------------
     
-def netflix_avg_user (user_id, rating, d, d1) :
-    uavg = 0
-    if ((len(d) == 0) or (user_id not in d)) :
-        d[user_id] = [rating, 1, rating]
-        d1[user_id] = rating
-        uavg = rating
-    else :
-        t0 = d[user_id][0]
-        sum_user = int(t0) + int(rating)
-        t1 = d[user_id][1]
-        count_movies = int(t1) + 1
-        uavg = sum_user / float(count_movies)
-        d[user_id][0] = sum_user
-        d[user_id][1] = count_movies
-        d[user_id][2] = uavg
-        d1[user_id] = uavg
-    return uavg
+def netflix_avg_user (user_id) :
+    '''
+        param: user_id
+        description: The variable data has the access to the cache.
+                     (keys,values) = (user ID,user's average)
+        return: The overall average of the user corresponding tot he user_id
+    '''
+    assert user_id != {}
+    url = "http://www.cs.utexas.edu/~ebanner/netflix-tests/ezo55-Average_Viewer_Rating_And_True_Variance_Cache.json"
+    r = urllib.request.urlopen(url)
+    data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
+    
+    avg = data[user_id][0]
 
+    assert 1.0 <= avg <= 5.0
+    return avg
 
 # ------------
-# netflix_read_movie_files
+# netflix_rating
 # ------------
 
-def netflix_read_movie_files (movie_id) :
-    """
+def netflix_rating (movie_id, user_id):
+    '''
+        param: movie_id, user_id
+        description: The variable data has the access to the cache.
+                     (movie: {user: rate}) 
+        return the average of the movie 
+    '''
+    assert user_id != {}
 
-    """
-    file_name = 'mv_' + (('0' * (7 - len(str(movie_id)))) + str(movie_id)) + '.txt'
-    # movie_file = open("/media/sf_Work/GitHub/private/cs373-netflix/" + file_name)
-    movie_file = open("/u/downing/cs/netflix/training_set/" + file_name)
-    # movie_file = urllib.request.urlopen('http://www.cs.utexas.edu/users/downing/netflix/training_set/' + file_name)
+    url = "http://www.cs.utexas.edu/~ebanner/netflix-tests/pam2599-probe_solutions.json"
+    r = urllib.request.urlopen(url)
+    data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
 
-    return movie_file
+    #optains tuple with the user_id and their corresponding ratings
+    d = data[movie_id]
+    rating = 0
+    #loop through the values in d and obtain the rating of the user_id
+    for user,rate in d.items() :
+        if user_id in user :
+            rating = rate
 
+    assert 0 <= rating <= 5.0
 
+    return rating
+
+# ------------
+# netflix_read_movie_id
+# ------------
+
+#return the movie_id from the line obtain from the input file
 def netflix_read_movie_id (line) :
+    assert line != {}
     return line[:-2]
 
+# ------------
+# netflix_read_user_id
+# ------------
+
+#return the user_id from the line obtain from the input file
 def netflix_read_user_id(line) :
+    assert line != {}
     return line[:-1]
-
-def netflix_read_rating(line) :
-    return line[-13:-12]
-
-def netflix_write_data_file (line) :
-    with open("data.txt",'a+') as f: 
-        f.write(line)
-    return f
-
-# -------------
-# netflix_print
-# -------------
-
-def netflix_print (user_id, predicted) :
-    """
-    
-    """
-    f = open("result.txt",'a+')
-    return f
 
 # -------------
 # netflix_solve
@@ -99,42 +108,60 @@ def netflix_solve (r, w) :
     r a reader
     w a writer
     """
-    d = dict()
-    d1 = dict()
+    
     movie_id = -1
+    tavg = 0 
     tsum = 0
-    tcount = 0
-    tsum2 = 0
+    count = 0
+    dt = dict()
+
+    #loop through the file and obtain the total average
+    for l in r :
+        if (l[len(l)-2] != ':') :
+            user_id = netflix_read_user_id(l)
+            uavg = netflix_avg_user (user_id)
+            dt.update({user_id:uavg})
+            tavg = tavg + uavg
+            count = count + 1
+
+    tavg = tavg/count
+    assert 1.0 <= tavg <= 5.0
+
+    #move the counter to the start of the file
+    r.seek(0)
+
+
     for s in r :
+      
         if (s[len(s)-2] == ':') :
+           #obtains the movie ID from the file 
+           #and calls the corresponding cache to get 
+           #the movie's average
             movie_id = netflix_read_movie_id(s)
-            t = netflix_avg_movie (movie_id)
-            netflix_write_data_file (s)
+            w.write(str(movie_id) + ':\n')
+            mavg = netflix_avg_movie (movie_id)
         else :
+            #obtains the user ID from the file. 
+            #Calls the corresponding cache to get 
+            #the user's average
             user_id = netflix_read_user_id(s)
-            movie_file = netflix_read_movie_files (movie_id)
-            t2 = movie_file.readline()
-            t2 = movie_file.readline()
-            while t2 != '\n' and t2 != '' : 
-                if (user_id) in t2 :
-                    rating = int(netflix_read_rating(t2))
-                    uavg = netflix_avg_user (user_id, rating, d, d1)
-                    predicted = (t + uavg / float(2))
-                    tcount = tcount + 1
-                    print(tcount)
-                    f1 = netflix_print(user_id, predicted)
-                    t5 = float(rating - predicted)
-                    t6 = abs(t5)
-                    f1.write(str(user_id) + ',' + str(predicted) + '\n')
-                    math.pow(t6, 2)
-                    tsum += t6
-                    tsum2 += predicted
-                    netflix_write_data_file (t2)
-                t2 = movie_file.readline()
-            movie_file.close()
-    t1 = r.readline()
-    t7 = tsum / (float(tcount))
-    rmse = 1 / float(1.0)
+            uavg = dt[user_id]
+            actual_rating = netflix_rating(movie_id, user_id)
+            #our prediction for the user's rating
+            prediction = (uavg * 0.521) + (mavg * 0.52) - 0.14
+            temp = prediction
+            temp = round(temp,1)
+            w.write(str(temp) + '\n')
+
+            #means square error calculation
+            value = float(actual_rating - prediction)
+            tsum += math.pow(value, 2)
+    
+   
+    #rmse calculation 
+    t7 = tsum / (float(count))
     rmse = math.sqrt(t7)
-    f1.write('RMSE = ' + str(rmse))
-    print(str(rmse))
+    rmse = round(rmse, 2)
+    assert rmse > 0
+    w.write('RMSE = ' + str(rmse) + '\n')
+    return rmse
--- TestNetflix.py
+++ TestNetflix.py
@@ -7,10 +7,10 @@
 # imports
 # -------
 
-import io
+from io import StringIO
 from unittest import main, TestCase
 
-from Netflix import  netflix_solve ,netflix_print,netflix_write_data_file,netflix_read_rating,netflix_read_user_id,netflix_read_movie_id ,netflix_avg_movie, netflix_avg_user,netflix_read_movie_files  
+from Netflix import  netflix_avg_movie, netflix_avg_user, netflix_read_movie_id, netflix_read_user_id, netflix_rating, netflix_solve
 
 # -----------
 # TestNetflix
@@ -19,72 +19,158 @@ from Netflix import  netflix_solve ,netflix_print,netflix_write_data_file,netfli
 class TestNetflix (TestCase) :
 
     # ------------
-    #  test_movie_avg
+    #  test_netflix_avg_movie
     # ------------
 
-    def test_movie_avg_1(self) :
-        s = "0000001"
-        r_avg = ((28)+(31*2)+(136*3)+(207*4)+(145*5))/547;
+    def test_netflix_avg_movie_1(self) :
+        s = "1"
+        r_avg = 3.749542961608775
         avg = netflix_avg_movie(s)
-        print(avg)
         self.assertEqual(avg,r_avg)
     
-    def test_movie_avg_2(self) :
-        s = "0000036"
-        r_avg = ((109)+(148*2)+(387*3)+(205*4)+(90*5))/939;
+    def test_netflix_avg_movie_2(self) :
+        s = "2"
+        r_avg = 3.5586206896551724
         avg = netflix_avg_movie(s)
         self.assertEqual(avg,r_avg)
 
-    def test_movie_avg_3(self) :
-        s = "0008686"
-        r_avg = ((27)+(28*2)+(70*3)+(23*4)+(6*5))/154;
+    def test_netflix_avg_movie_3(self) :
+        s = "17770"
+        r_avg = 2.816503800217155
         avg = netflix_avg_movie(s)
         self.assertEqual(avg,r_avg)
 
-    def test_movie_avg_4(self) :
-        s = "0013581"
-        r_avg = ((75)+(56*2)+(253*3)+(404*4)+(317*5))/1105;
+    def test_netflix_avg_movie_4(self) :
+        s = "8274"
+        r_avg = 3.887323943661972
         avg = netflix_avg_movie(s)
         self.assertEqual(avg,r_avg)
 
     #--------
-    # test_user_avg
+    # test_netflix_avg_user
     #--------
 
-
-    def test_user_avg_1(self) :
-        dic = dict()
-        initial_len = len(dic)
-        dic1 = dict()
-        user_id = '1010112'
-        rating = 5
-        user_avg = netflix_avg_user(user_id,rating,dic,dic1)
+    def test_netflix_avg_user_1(self) :
+        user_id = '378466'
+        rating = 4.4515539305301646
+        user_avg = netflix_avg_user(user_id)
         self.assertEqual(user_avg, rating)
-        self.assertEqual(dic[user_id][0], rating)
-        self.assertEqual(dic[user_id][1], 1)
-        self.assertEqual(dic[user_id][2], rating)
-        self.assertEqual(len(dic), initial_len+1)
 
-    def test_user_avg_2(self) :
-        dic = {'1010112' :[5,1,5],'156078': [3,1,3]}
-        dic1 = {'156078': 3}
-        user_id = '156078'
-        rating = 2
-        avg = netflix_avg_user(user_id,rating,dic,dic1)
-        self.assertEqual(dic[user_id][0], 5)
-        self.assertEqual(dic[user_id][1], 2)
-        self.assertEqual(dic[user_id][2], 2.5)
+    def test_netflix_avg_user_2(self) :
+        user_id = '1197733'
+        rating = 3.5
+        user_avg = netflix_avg_user(user_id)
+        self.assertEqual(user_avg, rating)
 
-    def test_user_avg_3(self) :
-        dic = {'156078': [5,2,2.5], '2558337' :[22,6,4.3333333], '1936287' : [1,1,1],'1122423' : [34,10,3.4], '8163045' : [20,7,2.85714286]}
-        dic1 = {'156078': 2.5, '2558337' :4.3333333, '1936287' : 1 ,'1122423' : 3.4, '8163045' : 2.85714286}
-        user_id = '1122423'
-        rating = 4
-        avg = netflix_avg_user(user_id,rating,dic,dic1)
-        self.assertEqual(dic[user_id][0], 38)
-        self.assertEqual(dic[user_id][1], 11)
-        self.assertEqual(dic[user_id][2], (38/11))
-  
+    def test_netflix_avg_user_3(self) :
+        user_id = '585188'
+        rating = 3.9285714285714284
+        user_avg = netflix_avg_user(user_id)
+        self.assertEqual(user_avg, rating)
+
+    #--------------------
+    # test_read_movie_id
+    #-------------------
+
+    def test_read_movie_id_1 (self) :
+        line = str("17757:\n")
+        ans  = "17757"
+        result = netflix_read_movie_id (line) 
+        self.assertEqual(result,ans)
+
+    def test_read_movie_id_2 (self) :
+        line = "12:\n"
+        ans  = "12"
+        result = netflix_read_movie_id (line) 
+        self.assertEqual(result,ans)
+
+    def test_read_movie_id_3 (self) :
+        line = "9732:\n"
+        ans  = "9732"
+        result = netflix_read_movie_id (line) 
+        self.assertEqual(result,ans)
+
+    def test_read_movie_id_4 (self) :
+        line = "73:\n"
+        ans  = "73"
+        result = netflix_read_movie_id (line) 
+        self.assertEqual(result,ans)
+
+    def test_read_movie_id_5 (self) :
+        line = "19378:\n"
+        ans  = "19378"
+        result = netflix_read_movie_id (line) 
+        self.assertEqual(result,ans)
+
+
+    #--------------------
+    # netflix_read_user_id 
+    #-------------------
+
+    def test_read_user_id_1(self) :
+        line = "716091\n"
+        ans  = "716091"
+        result = netflix_read_user_id (line) 
+        self.assertEqual(result,ans)
+
+    def test_read_user_id_2(self) :
+        line = "1242432\n"
+        ans  = "1242432"
+        result = netflix_read_user_id (line) 
+        self.assertEqual(result,ans)
+
+    def test_read_user_id_3(self) :
+        line = "1646405\n"
+        ans  = "1646405"
+        result = netflix_read_user_id (line) 
+        self.assertEqual(result,ans)
+    
+    def test_read_user_id_4(self) :
+        line = "2565022\n"
+        ans  = "2565022"
+        result = netflix_read_user_id (line) 
+        self.assertEqual(result,ans)
+
+    def test_read_user_id_5(self) :
+        line = "2095153\n"
+        ans  = "2095153"
+        result = netflix_read_user_id (line) 
+        self.assertEqual(result,ans)
+
+    #--------------------
+    # netflix_rating
+    #-------------------
+
+    def test_netflix_rating_1(self) :
+        movie_id = "4446"
+        user_id = "1657689"
+        result = netflix_rating(movie_id, user_id)
+        ans = 3
+        self.assertEqual(result,ans)
+
+    def test_netflix_rating_2(self) :
+        movie_id = "8082"
+        user_id = "2639376"
+        result = netflix_rating(movie_id, user_id)
+        ans = 5
+        self.assertEqual(result,ans)
+
+    def test_netflix_rating_3(self) :
+        movie_id = "16429"
+        user_id = "1002523"
+        result = netflix_rating(movie_id, user_id)
+        ans = 3
+        self.assertEqual(result,ans)
+
+    #--------------------
+    # netflix_solve
+    #-------------------
+
+    def test_netflix_solve (self) :
+        w = StringIO()
+        r = StringIO("1:\n1283744\n10:\n1952305\n1531863\n1000:\n2326571\n")
+        result = netflix_solve (r, w)
+        self.assertTrue(result < 1.0)
 
 # ----
 # main
